14b4a4091bf0cd9c244c3d10deb08384c38f6ee9
Jirka-Jinda <jindajirka@gmail.com>
2022-11-07 11:38:07 +0100

    test-app

---
+++ Mepis_rozcestnik/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mepis_rozcestnik.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
